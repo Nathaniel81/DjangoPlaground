6fbe084e38dbe030bf6aad7703fe3a677055bd66
Nathaniel81 <nathanield356@gmail.com>
2023-11-14 18:25:54 +0300

    ncnb

---
+++ blog/core/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class UniForm(forms.Form):
+    COURSE_CHOICES = (
+		(1, 'web development'),
+		(2, 'System Programming'),
+		(3, 'Data Science')
+	)
+    
+    name = forms.CharField()
+    age = forms.IntegerField()
+    subject = forms.ChoiceField(choices=COURSE_CHOICES)
+    date_of_birth = forms.DateField()
--- blog/core/views.py
+++ blog/core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .forms import UniForm
 # Create your views here.
+def index(request):
+    return render(request, 'core/index.html', {'form': UniForm()})
+
