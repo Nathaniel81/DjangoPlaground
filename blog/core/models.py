82d3d15bf8d159ab428180b6910a01e1674a36b0
Nathaniel81 <nathanield356@gmail.com>
2023-11-14 18:12:03 +0300

    ncnb

--- blog/core/models.py
+++ blog/core/models.py
@@ -51,4 +51,4 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return self.title
