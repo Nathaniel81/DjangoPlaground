0db866c9980b7dcadba12bfa51be4a71778b4547
Nathaniel81 <nathanield356@gmail.com>
2023-11-14 10:24:50 +0300

    nc

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Post(models.Model):
@@ -8,3 +10,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    user = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+class Comment(models.Model):
+    title = models.CharField(max_length=300)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
