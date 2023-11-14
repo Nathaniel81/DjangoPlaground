a2566459f979a03db9991abee4e65cdf6af56a75
Nathaniel81 <nathanield356@gmail.com>
2023-11-14 14:10:24 +0300

    ncnb

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -23,7 +24,12 @@ class Comment(models.Model):
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
