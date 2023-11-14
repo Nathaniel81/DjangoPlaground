1e72055b8f25f822b1b2fea83dd4d2f2100cf3a6
Nathaniel81 <nathanield356@gmail.com>
2023-11-14 11:59:07 +0300

    ncnb

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-
+ 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
